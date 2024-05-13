c7f1f1bee1d5f9f5e27fc4e71ee2ebcf4492b50f
Anjali <157092511+iamanjalibhat@users.noreply.github.com>
2024-05-13 23:23:35 +0530

    Update main.tf

--- scripts/main.tf
+++ scripts/main.tf
@@ -105,11 +105,11 @@ resource "null_resource" "local_command" {
   }
    
    provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/monitring-deployment.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/monitoring-deployment.yml"
   }
 
    provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/k8sdeploy.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/deployservice.yml"
   }
   depends_on = [aws_instance.kubernetes_worker_2]
 
@@ -137,7 +137,7 @@ resource "aws_instance" "monitoring_server" {
         command = " echo ${aws_instance.monitoring_server.public_ip} > inventory "
   }
    provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/monitroing.yml "
+  command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/monitoring.yml "
   }
 depends_on = [null_resource.local_command]
    
