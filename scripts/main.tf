232c359c3cdc1cc7698c4b33fc78c57d9927ddec
Anjali <157092511+iamanjalibhat@users.noreply.github.com>
2024-05-14 13:01:26 +0530

    Update main.tf

--- scripts/main.tf
+++ scripts/main.tf
@@ -1,15 +1,15 @@
-resource "tls_private_key" "capstone-key" {
+resource "tls_private_key" "new-key" {
   algorithm = "RSA"
 }
 
 resource "aws_key_pair" "app-key" {
-  key_name   = "capstone-key"
-  public_key = tls_private_key.capstone-key.public_key_openssh
+  key_name   = "new-key"
+  public_key = tls_private_key.new-key.public_key_openssh
 }
 
-resource "local_file" "capstone-key" {
-  content  = tls_private_key.capstone-key.private_key_pem
-  filename = "capstone-key.pem"
+resource "local_file" "new-key" {
+  content  = tls_private_key.new-key.private_key_pem
+  filename = "new-key.pem"
 
   provisioner "local-exec" {
     command = "chmod 600 ${self.filename}"
@@ -20,10 +20,10 @@ resource "local_file" "capstone-key" {
 resource "aws_instance" "kubernetes_master" {
   ami             = "ami-0e001c9271cf7f3b9"
   instance_type   = "t2.micro"
-  key_name        = "capstone-key"
+  key_name        = "new-key"
   vpc_security_group_ids= ["sg-011b20abb569c9149"]
   tags = {
-    Name = "kubernetes_master"
+    Name = "kubernetes_master" 
   }
 
   provisioner "remote-exec" {
@@ -32,25 +32,24 @@ resource "aws_instance" "kubernetes_master" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = tls_private_key.capstone-key.private_key_pem
+    private_key = tls_private_key.new-key.private_key_pem
     host        = self.public_ip
   }
    provisioner "local-exec" {
-        command = " echo ${aws_instance.kubernetes_master.public_ip} > inventory"
+        command = " echo ${aws_instance.kubernetes_master.public_ip} > inventory "
   }
    provisioner "local-exec" {
   	command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/scripts/kubernetes_master_setup.yaml"
   }
-   provisioner "local-exec" {
+  provisioner "local-exec" {
     command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/scripts/monitoring_setup.yaml"
   }
-  
 }
 
 resource "aws_instance" "kubernetes_worker1" {
   ami             = "ami-0e001c9271cf7f3b9"
   instance_type   = "t2.micro"
-  key_name        = "capstone-key"
+  key_name        = "new-key"
   vpc_security_group_ids= ["sg-011b20abb569c9149"]
   tags = {
     Name = "kubernetes_worker1"
@@ -62,11 +61,11 @@ resource "aws_instance" "kubernetes_worker1" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = tls_private_key.capstone-key.private_key_pem
+    private_key = tls_private_key.new-key.private_key_pem
     host        = self.public_ip
   }
    provisioner "local-exec" {
-        command = " echo ${aws_instance.kubernetes_worker1.public_ip} > inventory"
+        command = " echo ${aws_instance.kubernetes_worker1.public_ip} > inventory "
   }
    provisioner "local-exec" {
        command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/scripts/kubernetes_worker_setup.yaml"
@@ -77,7 +76,7 @@ resource "aws_instance" "kubernetes_worker1" {
 resource "aws_instance" "kubernetes_worker2" {
   ami             = "ami-0e001c9271cf7f3b9"
   instance_type   = "t2.micro"
-  key_name        = "capstone-key"
+  key_name        = "new-key"
   vpc_security_group_ids= ["sg-011b20abb569c9149"]
   tags = {
     Name = "kubernetes_worker2"
@@ -89,11 +88,11 @@ resource "aws_instance" "kubernetes_worker2" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = tls_private_key.capstone-key.private_key_pem
+    private_key = tls_private_key.new-key.private_key_pem
     host        = self.public_ip
   }
    provisioner "local-exec" {
-        command = " echo ${aws_instance.kubernetes_worker2.public_ip} > inventory"
+        command = " echo ${aws_instance.kubernetes_worker2.public_ip} > inventory "
   }
    provisioner "local-exec" {
        command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/scripts/kubernetes_worker_setup.yaml"
@@ -104,7 +103,7 @@ resource "aws_instance" "kubernetes_worker2" {
 resource "null_resource" "local_command" {
   
    provisioner "local-exec" {
-        command = " echo ${aws_instance.kubernetes_master.public_ip} > inventory"
+        command = " echo ${aws_instance.kubernetes_master.public_ip} > inventory "
   }
    
    provisioner "local-exec" {
