23a05648555256373169193921ac0c46da0c510c
Anjali <157092511+iamanjalibhat@users.noreply.github.com>
2024-05-14 10:41:34 +0530

    Update main.tf

--- scripts/main.tf
+++ scripts/main.tf
@@ -1,15 +1,15 @@
-resource "tls_private_key" "new-key" {
+resource "tls_private_key" "medicure-key" {
   algorithm = "RSA"
 }
 
 resource "aws_key_pair" "app-key" {
-  key_name   = "new-key"
-  public_key = tls_private_key.new-key.public_key_openssh
+  key_name   = "medicure-key"
+  public_key = tls_private_key.medicure-key.public_key_openssh
 }
 
-resource "local_file" "new-key" {
-  content  = tls_private_key.new-key.private_key_pem
-  filename = "new-key.pem"
+resource "local_file" "medicure-key" {
+  content  = tls_private_key.medicure-key.private_key_pem
+  filename = "medicure-key.pem"
 
   provisioner "local-exec" {
     command = "chmod 600 ${self.filename}"
@@ -20,7 +20,7 @@ resource "local_file" "new-key" {
 resource "aws_instance" "kubernetes_master" {
   ami             = "ami-04b70fa74e45c3917"
   instance_type   = "t2.micro"
-  key_name        = "new-key"
+  key_name        = "medicure-key"
   vpc_security_group_ids= ["sg-090308876f85665e4"]
   tags = {
     Name = "kubernetes_master"
@@ -32,28 +32,28 @@ resource "aws_instance" "kubernetes_master" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = tls_private_key.new-key.private_key_pem
+    private_key = tls_private_key.medicure-key.private_key_pem
     host        = self.public_ip
   }
    provisioner "local-exec" {
-        command = " echo ${aws_instance.kubernetes_master.public_ip} > inventory "
+        command = " echo ${aws_instance.kubernetes_master.public_ip} > inventory"
   }
    provisioner "local-exec" {
-  	command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/k8s-master-setup.yml"
+  	command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/scripts/kubernetes_master_setup.yaml"
   }
    provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/monitoring.yml "
+    command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/scripts/monitoring_setup.yaml"
   }
   
 }
 
-resource "aws_instance" "kubernetes_worker_1" {
+resource "aws_instance" "kubernetes_worker1" {
   ami             = "ami-04b70fa74e45c3917"
   instance_type   = "t2.micro"
-  key_name        = "new-key"
+  key_name        = "medicure-key"
   vpc_security_group_ids= ["sg-090308876f85665e4"]
   tags = {
-    Name = "kubernetes_worker_1"
+    Name = "kubernetes_worker1"
   }
 
   provisioner "remote-exec" {
@@ -62,25 +62,25 @@ resource "aws_instance" "kubernetes_worker_1" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = tls_private_key.new-key.private_key_pem
+    private_key = tls_private_key.medicure-key.private_key_pem
     host        = self.public_ip
   }
    provisioner "local-exec" {
-        command = " echo ${aws_instance.kubernetes_worker_1.public_ip} > inventory "
+        command = " echo ${aws_instance.kubernetes_worker1.public_ip} > inventory"
   }
    provisioner "local-exec" {
-       command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/k8s-worker-setup.yml "
+       command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/scripts/kubernetes_worker_setup.yaml"
   }
   depends_on = [aws_instance.kubernetes_master]
 }
 
-resource "aws_instance" "kubernetes_worker_2" {
+resource "aws_instance" "kubernetes_worker2" {
   ami             = "ami-04b70fa74e45c3917"
   instance_type   = "t2.micro"
-  key_name        = "new-key"
+  key_name        = "medicure-key"
   vpc_security_group_ids= ["sg-090308876f85665e4"]
   tags = {
-    Name = "kubernetes_worker_2"
+    Name = "kubernetes_worker2"
   }
 
   provisioner "remote-exec" {
@@ -89,31 +89,31 @@ resource "aws_instance" "kubernetes_worker_2" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = tls_private_key.new-key.private_key_pem
+    private_key = tls_private_key.medicure-key.private_key_pem
     host        = self.public_ip
   }
    provisioner "local-exec" {
-        command = " echo ${aws_instance.kubernetes_worker_2.public_ip} > inventory "
+        command = " echo ${aws_instance.kubernetes_worker2.public_ip} > inventory"
   }
    provisioner "local-exec" {
-       command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/k8s-worker-setup.yml "
+       command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/scripts/kubernetes_worker_setup.yaml"
   }
-  depends_on = [aws_instance.kubernetes_worker_1]
+  depends_on = [aws_instance.kubernetes_worker1]
 }
 
 resource "null_resource" "local_command" {
   
    provisioner "local-exec" {
-        command = " echo ${aws_instance.kubernetes_master.public_ip} > inventory "
+        command = " echo ${aws_instance.kubernetes_master.public_ip} > inventory"
   }
    
    provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/monitoring-deployment.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/scripts/monitoring_deployment.yaml"
   }
 
    provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/k8sdeploy.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/scripts/kubernetes_deployment.yaml"
   }
-  depends_on = [aws_instance.kubernetes_worker_2]
+  depends_on = [aws_instance.kubernetes_worker2]
 
 }
