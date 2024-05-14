b67a02e1acd6a0643ceee685243c7f7ef1ff083f
Anjali <157092511+iamanjalibhat@users.noreply.github.com>
2024-05-14 12:22:26 +0530

    Update outputs.tf

--- scripts/outputs.tf
+++ scripts/outputs.tf
@@ -3,10 +3,10 @@ output "kubernetes_master_ip" {
   value = aws_instance.kubernetes_master.public_ip
 }
 
-output "kubernetes_worker_1_ip" {
-  value = aws_instance.kubernetes_worker_1.public_ip
+output "kubernetes_worker1_ip" {
+  value = aws_instance.kubernetes_worker1.public_ip
 }
 
-output "kubernetes_worker_2_ip" {
-  value = aws_instance.kubernetes_worker_2.public_ip
+output "kubernetes_worker2_ip" {
+  value = aws_instance.kubernetes_worker2.public_ip
 }
